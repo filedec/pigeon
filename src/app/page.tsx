7c4d974b434dc6c53b6ca1f7ebd6a8cbe08d9f49
nischaltimalsina <nischaltimalcna@gmail.com>
2024-02-15 21:14:53 +0545

    Init Repo

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <h2 className="text-2xl text-center">Filedec</h2> 
+    </main>
+  );
+}
